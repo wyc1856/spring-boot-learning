b9953b02c6ddd534a34a6ab59c9f5c2a9a057877
wyc1856 <wyc1856@gmail.com>
2020-02-09 19:32:00 +0800

    依赖注入

--- thinking-in-spring/dependency-lookup/src/main/java/club/wyc1856/dependency/ioc/injection/DependencyInjectionDemo.java
+++ thinking-in-spring/dependency-lookup/src/main/java/club/wyc1856/dependency/ioc/injection/DependencyInjectionDemo.java
@@ -1,7 +1,11 @@
 package club.wyc1856.dependency.ioc.injection;
 
+import club.wyc1856.dependency.ioc.repository.UserRepository;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.env.Environment;
 
 /**
  * 依赖注入
@@ -13,5 +17,32 @@ public class DependencyInjectionDemo {
 
     public static void main(String[] args) {
         BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-injection-context.xml");
+
+        //依赖来源一：自定义Bean
+        UserRepository userRepository = beanFactory.getBean(UserRepository.class);
+        System.out.println("自定义Bean：" + userRepository.getUsers());
+
+        //依赖来源二：内建依赖（只能通过依赖注入，不能通过依赖查找）
+        BeanFactory internalBeanFactory = userRepository.getBeanFactory();
+        System.out.println("内建依赖：" + internalBeanFactory);
+        System.out.println("内建依赖BeanFactory和自己声明的BeanFactory是否是相同的Bean：" + (internalBeanFactory == beanFactory));
+//        System.out.println("依赖查找内建依赖：" + lookupInternalDependency(beanFactory));
+
+        ObjectFactory<ApplicationContext> objectFactory = userRepository.getObjectFactory();
+        System.out.println("延迟查找注入的BeanFactory和自己声明的BeanFactory是否是相同的Bean：" + (objectFactory.getObject() == beanFactory));
+
+        //依赖来源三：内建Bean
+        Environment environment = beanFactory.getBean(Environment.class);
+        System.out.println(environment);
+    }
+
+    private static BeanFactory lookupInternalDependency(BeanFactory beanFactory) {
+        try {
+            return beanFactory.getBean(BeanFactory.class);
+        } catch (Exception e) {
+            System.err.println("依赖查找内建依赖失败：" + e.getMessage());
+            e.printStackTrace();
+        }
+        return null;
     }
 }
---
+++ thinking-in-spring/dependency-lookup/src/main/java/club/wyc1856/dependency/ioc/repository/UserRepository.java
@@ -0,0 +1,48 @@
+package club.wyc1856.dependency.ioc.repository;
+
+import club.wyc1856.dependency.ioc.lookup.domain.User;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Collection;
+
+/**
+ * 用户仓库
+ *
+ * @author wyc1856
+ * @date 2020/2/9
+ **/
+public class UserRepository {
+
+    private Collection<User> users; //自定义Bean
+
+    private BeanFactory beanFactory; //容器内建的依赖
+
+    private ObjectFactory<ApplicationContext> objectFactory;
+
+    public Collection<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Collection<User> users) {
+        this.users = users;
+    }
+
+    public BeanFactory getBeanFactory() {
+        return beanFactory;
+    }
+
+    public void setBeanFactory(BeanFactory beanFactory) {
+        this.beanFactory = beanFactory;
+    }
+
+    public ObjectFactory<ApplicationContext> getObjectFactory() {
+        return objectFactory;
+    }
+
+    public void setObjectFactory(ObjectFactory<ApplicationContext> objectFactory) {
+        this.objectFactory = objectFactory;
+    }
+
+}
