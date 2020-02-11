d9cc40cc247f9da20615143004aebf7349b201c0
wyc1856 <wyc1856@gmail.com>
2020-02-11 08:49:31 +0800

    BeanFactory和ApplicationContext分别作为Ioc容器

---
+++ thinking-in-spring/dependency-lookup/src/main/java/club/wyc1856/dependency/ioc/container/AnnotationApplicationContextAsIocContainerDemo.java
@@ -0,0 +1,48 @@
+package club.wyc1856.dependency.ioc.container;
+
+import club.wyc1856.dependency.ioc.lookup.domain.User;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Map;
+
+/**
+ * 注解类型的{@link ApplicationContext}作为Ioc容器示例
+ *
+ * @author wyc1856
+ * @date 2020/2/11
+ **/
+public class AnnotationApplicationContextAsIocContainerDemo {
+
+    public static void main(String[] args) {
+        //创建注解类型的ApplicationContext
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        //将当前类当做配置类进行加载
+        applicationContext.register(AnnotationApplicationContextAsIocContainerDemo.class);
+        //启动应用上下文
+        applicationContext.refresh();
+        //按类型查找bean集合
+        lookupCollectionByType(applicationContext);
+        //关闭应用上下文
+        applicationContext.close();
+    }
+
+    private static void lookupCollectionByType(BeanFactory beanFactory) {
+        if (beanFactory instanceof ListableBeanFactory) {
+            ListableBeanFactory listableBeanFactory = (ListableBeanFactory) beanFactory;
+            Map<String, User> beansOfType = listableBeanFactory.getBeansOfType(User.class);
+            System.out.println("User 类型的bean包括：" + beansOfType);
+        }
+    }
+
+    @Bean
+    public User annotationUser() {
+        User user = new User();
+        user.setId(1L);
+        user.setName("注解类型User");
+        return user;
+    }
+}
---
+++ thinking-in-spring/dependency-lookup/src/main/java/club/wyc1856/dependency/ioc/container/BeanFactoryAsIocContainerDemo.java
@@ -0,0 +1,39 @@
+package club.wyc1856.dependency.ioc.container;
+
+import club.wyc1856.dependency.ioc.lookup.domain.User;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+
+import java.util.Map;
+
+/**
+ * {@link BeanFactory} 作为Ioc容器示例
+ *
+ * @author wyc1856
+ * @date 2020/2/11
+ **/
+public class BeanFactoryAsIocContainerDemo {
+
+    public static void main(String[] args) {
+        //创建BeanFactory
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        //声明Xml资源读取器
+        XmlBeanDefinitionReader xmlBeanDefinitionReader = new XmlBeanDefinitionReader(beanFactory);
+        //加载Xml配置
+        String location = "classpath:/META-INF/dependency-lookup-context.xml";
+        int beanDefinitionCount = xmlBeanDefinitionReader.loadBeanDefinitions(location);
+        System.out.println("加载了" + beanDefinitionCount + "个beanDefinition");
+        //按类型查找bean集合
+        lookupCollectionByType(beanFactory);
+    }
+
+    private static void lookupCollectionByType(BeanFactory beanFactory) {
+        if (beanFactory instanceof ListableBeanFactory) {
+            ListableBeanFactory listableBeanFactory = (ListableBeanFactory) beanFactory;
+            Map<String, User> beansOfType = listableBeanFactory.getBeansOfType(User.class);
+            System.out.println("User 类型的bean包括：" + beansOfType);
+        }
+    }
+}
