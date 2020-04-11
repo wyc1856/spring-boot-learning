a317cb6048e8dbdd3ad18062ab35176019a1d886
wyc1856 <wyc1856@gmail.com>
2020-04-11 19:01:56 +0800

    集成 swagger2

--- web-api/src/main/java/club/wyc1856/web/demo/config/SwaggerConfiguration.java
+++ web-api/src/main/java/club/wyc1856/web/demo/config/SwaggerConfiguration.java
@@ -3,8 +3,10 @@ package club.wyc1856.web.demo.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -29,10 +31,19 @@ public class SwaggerConfiguration {
     public Docket docket() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .enable(enable)
+                .apiInfo(apiInfo())
                 .select()
                 // 这里配置要扫描的包,接口在哪个包就配置哪个包
                 .apis(RequestHandlerSelectors.basePackage("club.wyc1856.web.demo.controller"))
                 .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("接口文档")
+                .description("使用 Swagger2 生成接口文档")
+                .version("1.0")
                 .build();
     }
 }
--- web-api/src/main/java/club/wyc1856/web/demo/controller/UserController.java
+++ web-api/src/main/java/club/wyc1856/web/demo/controller/UserController.java
@@ -3,6 +3,7 @@ package club.wyc1856.web.demo.controller;
 import club.wyc1856.web.demo.entity.User;
 import club.wyc1856.web.demo.service.IUserService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -25,11 +26,13 @@ public class UserController {
     private IUserService userService;
 
     @GetMapping("get_user")
+    @ApiOperation("获取用户信息")
     public User getUser(){
         return userService.getUser();
     }
 
     @PostMapping("add_user")
+    @ApiOperation("添加新用户")
     public String addUser(@RequestBody @Valid User user){
         log.info("添加新用户:{}", user);
         return "success";
